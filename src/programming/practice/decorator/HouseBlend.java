8c1d385e5f89d58fe39cb48d98deabd853cc590c
NANNADA4 <68773516+NANNADA4@users.noreply.github.com>
2020-11-03 12:49:05 +0900

    Add Decorator

---
+++ src/programming/practice/decorator/HouseBlend.java
@@ -0,0 +1,14 @@
+package programming.practice.decorator;
+
+public class HouseBlend extends Beverage {
+
+    public HouseBlend() {
+        this.description = "하우스블렌드";
+    }
+
+    @Override
+    public double cost() {
+        return 2.0;
+    }
+
+}
